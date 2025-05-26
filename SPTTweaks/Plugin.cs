a97a83f93ce3d7e0d2ebd3a072f3c1845cb9b1cd
qe201020335 <qe201020335@sina.com>
2025-05-25 21:45:33 -0400

    Basic mod project setup

--- SPTTweaks/Plugin.cs
+++ SPTTweaks/Plugin.cs
@@ -1,5 +1,34 @@
-﻿namespace SPTTweaks;
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Models.External;
+using SPTarkov.Server.Core.Models.Utils;
 
-public class Plugin
+namespace SPTTweaks;
+
+[Injectable(InjectionType.Singleton)]
+[UsedImplicitly]
+internal class Plugin : IPreSptLoadModAsync, IPostSptLoadModAsync, IPostDBLoadModAsync
 {
+    private readonly ISptLogger<Plugin> _logger;
+
+    public Plugin(ISptLogger<Plugin> logger)
+    {
+        _logger = logger;
+    }
+
+    async Task IPreSptLoadModAsync.PreSptLoadAsync()
+    {
+        _logger.Info("SPTTweaks PreSptLoad");
+    }
+
+    async Task IPostSptLoadModAsync.PostSptLoadAsync()
+    {
+        _logger.Info("SPTTweaks PostSptLoad");
+    }
+
+    async Task IPostDBLoadModAsync.PostDBLoadAsync()
+    {
+        _logger.Info("SPTTweaks PostDBLoad");
+    }
 }
