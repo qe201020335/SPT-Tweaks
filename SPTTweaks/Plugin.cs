f5b8fff32ffd5292640ff22e12c055f40eaeb719
qe201020335 <qe201020335@sina.com>
2025-05-25 21:45:33 -0400

    Basic mod project setup

--- SPTTweaks/Plugin.cs
+++ SPTTweaks/Plugin.cs
@@ -1,5 +1,30 @@
-﻿namespace SPTTweaks;
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Models.External;
+using SPTarkov.Server.Core.Models.Utils;
 
-public class Plugin
+namespace SPTTweaks;
+
+[Injectable(InjectionType.Singleton)]
+public class Plugin: IPreSptLoadMod, IPostSptLoadMod, IPostDBLoadMod 
 {
+    private readonly ISptLogger<Plugin> _logger;
+    public Plugin(ISptLogger<Plugin> logger)
+    {
+        _logger = logger;
+    }
+    
+    void IPreSptLoadMod.PreSptLoad()
+    {
+        _logger.Info("SPTTweaks PreSptLoad");
+    }
+    
+    void IPostSptLoadMod.PostSptLoad()
+    {
+        _logger.Info("SPTTweaks PostSptLoad");
+    }
+    
+    void IPostDBLoadMod.PostDBLoad()
+    {
+        _logger.Info("SPTTweaks PostDBLoad");
+    }
 }
---
+++ SPTTweaks/PluginMetadata.cs
@@ -0,0 +1,19 @@
+﻿using SPTarkov.Server.Core.Models.Spt.Mod;
+
+namespace SPTTweaks;
+
+public record PluginMetadata : AbstractModMetadata
+{
+    public override string? Name { get; set; } = "SPTTweaks";
+    public override string? Author { get; set; } = "qe201020335";
+    public override List<string>? Contributors { get; set; }
+    public override string? Version { get; set; } = "2.0.0";
+    public override string? SptVersion { get; set; } = "~4.0.0";
+    public override List<string>? LoadBefore { get; set; }
+    public override List<string>? LoadAfter { get; set; }
+    public override List<string>? Incompatibilities { get; set; }
+    public override Dictionary<string, string>? ModDependencies { get; set; }
+    public override string? Url { get; set; } = "https://github.com/qe201020335/SPT-Tweaks";
+    public override bool? IsBundleMod { get; set; } = false;
+    public override string? Licence { get; set; } = "MIT";
+}
