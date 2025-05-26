06725352ee4923ffff2b12075225676b28075976
qe201020335 <qe201020335@sina.com>
2025-05-25 23:47:29 -0400

    Config loading

---
+++ SPTTweaks/Configuration/PluginConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SPTTweaks.Configuration;
+
+internal class PluginConfig
+{
+    public NetworkConfig Network { get; set; } = new();
+}
+
+internal abstract class DisableableConfig
+{
+    [JsonPropertyOrder(-99)]
+    public virtual bool Enable { get; set; }
+}
+
+internal class NetworkConfig : DisableableConfig
+{
+    public string ListenIp { get; set; } = "127.0.0.1";
+
+    public string BackendIp { get; set; } = "127.0.0.1";
+}
