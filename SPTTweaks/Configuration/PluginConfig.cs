c8129d3cebd9d273b4f35de48dec6b16d021098c
qe201020335 <qe201020335@sina.com>
2025-05-26 00:12:39 -0400

    Add network config

--- SPTTweaks/Configuration/PluginConfig.cs
+++ SPTTweaks/Configuration/PluginConfig.cs
@@ -1,6 +1,21 @@
-﻿namespace SPTTweaks.Configuration;
+﻿using System.Text.Json.Serialization;
+
+namespace SPTTweaks.Configuration;
 
 internal class PluginConfig
 {
-    //TODO: Add config values as features are ported
+    public NetworkConfig Network { get; set; } = new();
+}
+
+internal abstract class DisableableConfig
+{
+    [JsonPropertyOrder(-99)]
+    public virtual bool Enable { get; set; }
+}
+
+internal class NetworkConfig : DisableableConfig
+{
+    public string ListenIp { get; set; } = "127.0.0.1";
+
+    public string BackendIp { get; set; } = "127.0.0.1";
 }
--- SPTTweaks/Plugin.cs
+++ SPTTweaks/Plugin.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.Models.External;
+using SPTarkov.Server.Core.Models.Spt.Config;
 using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Servers;
+using SPTTweaks.Configuration;
 
 namespace SPTTweaks;
 
@@ -11,15 +14,29 @@ namespace SPTTweaks;
 internal class Plugin : IPreSptLoadModAsync, IPostSptLoadModAsync, IPostDBLoadModAsync
 {
     private readonly ISptLogger<Plugin> _logger;
+    private readonly PluginConfig _config;
 
-    public Plugin(ISptLogger<Plugin> logger)
+    private readonly ConfigServer _configServer;
+
+    public Plugin(ISptLogger<Plugin> logger, ConfigManager configManager, ConfigServer configServer)
     {
         _logger = logger;
+        _config = configManager.Config;
+
+        _configServer = configServer;
     }
 
     async Task IPreSptLoadModAsync.PreSptLoadAsync()
     {
         _logger.Info("SPTTweaks PreSptLoad");
+
+        if (_config.Network.Enable)
+        {
+            var httpConfig = _configServer.GetConfig<HttpConfig>();
+            httpConfig.Ip = _config.Network.ListenIp;
+            httpConfig.BackendIp = _config.Network.BackendIp;
+            _logger.Info($"[SPTTweaks] Using backend <{httpConfig.BackendIp}> listened on <{httpConfig.Ip}>");
+        }
     }
 
     async Task IPostSptLoadModAsync.PostSptLoadAsync()
